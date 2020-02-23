949181a64c627b2da87d4a93c9906e446d792780
Vincent Guizot <30890039+CenZo128@users.noreply.github.com>
2020-02-23 15:59:25 +0700

    upload

--- New folder/exercise-7.js
+++ New folder/exercise-7.js
@@ -1,8 +1,18 @@
 function hitungJumlahKata(kalimat) {
   // you can only write your code here!
-  var result=kalimat.split(" ").length;
+  var result=[]
+  var temp=''
+  for(var i=0;i<kalimat.length;i++){
+    if(kalimat[i]==" " || i==kalimat.length-1)
+    {
+      result.push(temp)
+      temp=""
+    }else{
+      temp+=kalimat[i]
+    }
+  }
 
-  return result
+  return result.length
 }
 
 // TEST CASES
@@ -10,4 +20,4 @@ console.log(hitungJumlahKata('I have a dream')); // 4
 console.log(hitungJumlahKata('Never eat shredded wheat or cake')); // 6
 console.log(hitungJumlahKata('A song to sing')); // 4
 console.log(hitungJumlahKata('I')); // 1
-console.log(hitungJumlahKata('I believe I can code')); // 5
+console.log(hitungJumlahKata('I believe I can code')); //5
